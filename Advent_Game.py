fc3cff3b7b078d4ea33a053f679dff77df81c288
George Khem <62788774+gpkhem@users.noreply.github.com>
2020-04-16 10:24:51 -0700

    Add files via upload

--- Advent_Game.py
+++ Advent_Game.py
@@ -5,34 +5,38 @@ import random
 def print_pause(print_message):
     print(print_message)
     time.sleep(1)
-    
+
+
 def introduction(item, option):
     print_pause("You find yourself standing inside a front door of a open gym "
-            "with basketball courts.\n")
+                "with basketball courts.\n")
     print_pause("Rumor has it that a " + option + " is somewhere around "
-            "here, and beating everyone in one on one.\n")
+                "here, and beating everyone in one on one.\n")
     print_pause("In front of you is a open court.\n")
-    print_pause("To your right is a court occuppied by a tall unrecognizable player.\n")
+    print_pause("To your right is a court occuppied by"
+                "a tall unrecognizable player.\n")
     print_pause("On your feet is your trusty but beatup basketball shoes.\n")
+
 
 def open_court(item, option):
     if "gymbag" in item:
         print_pause("\nYou walk to the empty court.")
         print_pause("\nYou've been here before, and gotten all"
-                " the swag. It's just an empty court"
-                " now.")
+                    " the swag. It's just an empty court"
+                    " now.")
         print_pause("\nYou walk back to the frontdoor.\n")
     else:
         print_pause("\nYou approach the empty court.")
         print_pause("\nIt turns out to there was a shoe box.")
         print_pause("\nYou open the box and shock to see "
-                "the shoes.")
+                    "the shoes.")
         print_pause("\nIt is some Air Jordan Is!!!!")
         print_pause("\nYou take off your shoes and put on "
-                "the new shoes.")
+                    "the new shoes.")
         print_pause("\nYou walk back to the front door.\n")
         item.append("gymbag")
     front_door(item, option)
+
 
 def right_court(item, option):
     print_pause("\nYou approach the player.")
@@ -52,7 +56,7 @@ def right_court(item, option):
                 print_pause("\n" + option + " takes one look at you "
                             "and gives you a head nod!")
                 print_pause("\nYou have got the respect of " + option +
-                            ". You are victorious!\n")
+                            ". Sounds like a WIN to me!\n")
             else:
                 print_pause("\nYou do your best...")
                 print_pause("but your footwear is no match for "
@@ -84,20 +88,20 @@ def front_door(item, option):
 def play_again():
     again = input("Would you like to play again? (y/n)").lower()
     if again == "y":
-        print_pause("\n\n\nExcellent! Lets go champ ...\n\n\n")
+        print_pause("\n\nExcellent! Lets go champ ...\n\n")
         play_game()
     elif again == "n":
-        print_pause("\n\n\nThanks for playing!.\n\n\n")
+        print_pause("\n\nThanks for playing!.\n\n")
     else:
         play_again()
 
 
 def play_game():
     item = []
-    option = random.choice(["Michael Jordan", "Kobe Bryant", "Shaq", "Lebron James"])
+    option = random.choice(["Michael Jordan", "Kobe Bryant",
+                            "Shaq", "Lebron James"])
     introduction(item, option)
     front_door(item, option)
 
 
 play_game()
-
